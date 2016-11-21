82c331838d944c68c62ab232a9288463a4e19ffa
Daniel <Daniel@DJB-PC.home>
2016-11-21 14:33:02 -0500

    Added Stuff

--- src/NavViewSplashPanel.java
+++ src/NavViewSplashPanel.java
@@ -10,7 +10,7 @@ public class NavViewSplashPanel extends JPanel{
     JLabel optionsLabel;
     NavViewSplashPanel()
     {
-            splashButton = new JButton(new ImageIcon("src/images/BoxJumpSplash.png"));            
+            splashButton = new JButton(new ImageIcon("src/images/BoxJumpSplash.jpg"));            
             add(splashButton);
             
             optionsLabel = new JLabel("No options selected yet.");
