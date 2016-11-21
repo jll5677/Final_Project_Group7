12609d9ec15337fa58f32f0335159c7700becf55
Daniel <Daniel@DJB-PC.home>
2016-11-21 14:34:27 -0500

    Added Stuff

--- src/MainView.java
+++ src/MainView.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -11,7 +12,7 @@ import javax.swing.JPanel;
 public class MainView extends JPanel{
     MainView()
     {
-        JButton welcomeButton = new JButton("Welcome to the Main View!");
+        JLabel welcomeButton = new JLabel("Welcome to the Main View!");
         add(welcomeButton);
     }
 }
