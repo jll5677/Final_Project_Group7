0e2dbc02dc8b0e87ceda9ef3b82745ae81b89b47
Daniel <Daniel@DJB-PC.home>
2016-11-23 15:46:03 -0500

    Added Stuff

--- src/MainController.java
+++ src/MainController.java
@@ -11,17 +11,14 @@ public class MainController {
         this.m_model = m_model;
         this.m_view = m_view;
         
-        final OptionsModel o_model = new OptionsModel();
-        final OptionsView o_view = new OptionsView(o_model);
-        
-        class ButtonListener implements ActionListener {
+       class ButtonListener implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 JButton clickSource = (JButton)e.getSource();
                 if(clickSource == m_view.updatePlayer1Label) {
-                    m_view.player1TempLabel.setText("PLAYER 1 INFO - Name: " + o_model.getPlayer1Name() + " Color: " + o_model.getPlayer1ColorString());
+                    m_view.player1TempLabel.setText("PLAYER 1 INFO - Name: " + m_model.getPlayer1Name() + " Color: " + m_model.getPlayer1ColorString());
                 }
                 if(clickSource == m_view.updatePlayer2Label) {
-                    m_view.player2TempLabel.setText("PLAYER 2 INFO - Name: " + o_model.getPlayer2Name() + " Color: " + o_model.getPlayer2ColorString());
+                    m_view.player2TempLabel.setText("PLAYER 2 INFO - Name: " + m_model.getPlayer2Name() + " Color: " + m_model.getPlayer2ColorString());
                 }
             }
         }
