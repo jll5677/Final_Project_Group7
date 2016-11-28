6da212af561346cbf2df784aafcb23743185ae96
Jimmy <Jimmy@Jimmy-PC.hsd1.pa.comcast.net>
2016-11-27 23:53:47 -0500

    new commit

--- src/InstructionView.java
+++ src/InstructionView.java
@@ -14,7 +14,16 @@ import javax.swing.JPanel;
  */
 public class InstructionView extends JPanel{
     InstructionView(){
-        JLabel info = new JLabel("To move the character(box) player one uses left");
+        JLabel info = new JLabel("The objective of the game is to get to the yellow platform on the other side by moving the square");
         add(info);
+        
+        JLabel info1 = new JLabel("There will be a timer and the palyer has to get to the otherside in the shortest amount of time possible.");
+        add(info1);
+        
+        JLabel info2 = new JLabel("Player one uses left and right arrow keys to move and up arrow key to jump.");
+        add(info2);
+        
+        JLabel info3 = new JLabel("Player two would use the keys A and D to move and W to jump.");
+        add(info3);
     }
 }
