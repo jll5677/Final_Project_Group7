657e9aaac23db8a5116f8e067518e4465dd2c5bd
Daniel <Daniel@104.39.60.143>
2016-11-30 15:23:21 -0500

    Added Stuff

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,8 +1,9 @@
 
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class GamePlay {
+public class GamePlay extends JPanel{
     JButton P1GamePiece;
     JButton P2GamePiece;
     Color P1Color;
@@ -16,5 +17,11 @@ public class GamePlay {
         P1GamePiece.setBackground(P1Color);
         P2Color = m_model.getPlayer2Color();
         P2GamePiece.setBackground(P2Color);
+        
+        add(P1GamePiece);
+        add(P2GamePiece);
+        
+        setBackground(Color.BLACK);
+        setLayout(null);
     }
 }
