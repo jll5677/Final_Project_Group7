5755fa05d36bdad03adceb0ad837458133e87824
Daniel <Daniel@104.39.60.143>
2016-11-30 15:45:50 -0500

    Added Stuff

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,7 +1,10 @@
-
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class GamePlay extends JPanel{
     JButton P1GamePiece;
@@ -10,7 +13,22 @@ public class GamePlay extends JPanel{
     Color P2Color;
     MainModel m_model;
     
+    private final int WIFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+
+    private final String MOVE_P1_RIGHT = "Move P1 Right";
+    private final String MOVE_P1_LEFT = "Move P1 Left";
+    private final String MOVE_P2_RIGHT = "Move P2 Right";
+    private final String MOVE_P2_LEFT = "Move P2 Left";
+    
+    private final int P1_RIGHT = 1;
+    private final int P1_LEFT = 2;
+    private final int P2_RIGHT = 1;
+    private final int P2_LEFT = 2;
+    
     GamePlay(){
+        setBackground(Color.BLACK);
+        setLayout(null);
+        
         P1GamePiece = new JButton();
         P2GamePiece = new JButton();
         P1Color = m_model.getPlayer1Color();
@@ -23,7 +41,17 @@ public class GamePlay extends JPanel{
         add(P1GamePiece);
         add(P2GamePiece);
         
-        setBackground(Color.BLACK);
-        setLayout(null);
+        P1GamePiece.getInputMap(WIFW).put(KeyStroke.getKeyStroke("Right"), MOVE_P1_RIGHT);
+        P1GamePiece.getInputMap(WIFW).put(KeyStroke.getKeyStroke("Left"), MOVE_P1_LEFT);
+        
+        P2GamePiece.getInputMap(WIFW).put(KeyStroke.getKeyStroke("Right"), MOVE_P2_RIGHT);
+        P2GamePiece.getInputMap(WIFW).put(KeyStroke.getKeyStroke("Left"), MOVE_P2_LEFT);
+    }
+    
+    private class MoveAction extends AbstractAction {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
     }
 }
