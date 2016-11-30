7d717396bc6f0c9a2192e6adaf4fd8c5ded64d16
Daniel <Daniel@104.39.60.143>
2016-11-30 15:13:56 -0500

    Added Stuff

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,13 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Daniel
- */
+import java.awt.Color;
+import javax.swing.JButton;
+
 public class GamePlay {
+    JButton P1GamePiece;
+    JButton P2GamePiece;
+    Color P1Color;
+    Color P2Color;
+    MainModel m_model;
     
+    GamePlay(){
+        P1GamePiece = new JButton();
+        P2GamePiece = new JButton();
+        P1GamePiece.setBackground(P1Color);
+        P2GamePiece.setBackground(P2Color);
+    }
+    
+    public void setP1Color() {
+        P1Color = m_model.getPlayer1Color();
+    }
+    public void setP2Color() {
+        P2Color = m_model.getPlayer2Color();
+    }
 }
