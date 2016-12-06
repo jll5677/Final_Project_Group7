4291b4dfec8532ec685dbb0b3206981daae727b1
Daniel <Daniel@104.39.67.170>
2016-12-06 14:05:40 -0500

    Added Stuff

--- src/InstructionView.java
+++ src/InstructionView.java
@@ -11,10 +11,7 @@ public class InstructionView extends JPanel{
         JLabel info1 = new JLabel("There will be a timer and the player has to get to the other side in the shortest amount of time possible.");
         add(info1);
         
-        JLabel info2 = new JLabel("Player one uses left and right arrow keys to move and up arrow key to jump.");
+        JLabel info2 = new JLabel("The player uses the left and right arrow keys to move and the up arrow key to jump.");
         add(info2);
-        
-        JLabel info3 = new JLabel("Player two would use the keys A (left) and D (right) to move and W to jump.");
-        add(info3);
     }
 }
